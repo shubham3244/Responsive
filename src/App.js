f61bced8eda56870031e99b03224770e091d2739
shubham3244 <89273033+shubham3244@users.noreply.github.com>
2021-10-28 01:55:57 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Responsive from "./responsive";
+
+const App = () => {
+  return (
+    <>
+      <Responsive />
+    </>
+  );
+};
+export default App;
---
+++ src/responsive.js
@@ -0,0 +1,11 @@
+import React from "react";
+const Responsive = () => {
+  return (
+    <>
+      <div>
+        <h1>Bookpod.</h1>
+      </div>
+    </>
+  );
+};
+export default Responsive;
